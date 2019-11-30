4d6e5140dd850bfa2c2881d67a60d2138dfd3288
Ricozero <735784247@qq.com>
2019-11-30 22:20:43 +0800

    模型预测脚本修改为指定多个文件的方式

--- run_test.sh
+++ run_test.sh
@@ -4,36 +4,40 @@ start_epoch=90
 end_epoch=150
 interval=20
 
-echo "start testing..."
+echo "Start testing..."
 date
 for i in $(seq ${start_epoch} ${interval} ${end_epoch})
 do
-	model=snapshot/${model_name}/${i}.pkl
+	model_path=snapshot/${model_name}/${i}.pkl
 	save_dir=results/${model_name}/epoch${i}
 	mkdir -p ${save_dir}
 	mkdir -p ${save_dir}/temp
-	gpuids=(0 1)
-	for j in $(seq 1 3)
+	img_paths="dataset/Tairi_Google_20_small1.png
+		dataset/Tairi_4cm_small1_small.png
+		dataset/Tairi_2cm_small1_small.png"
+	for img_path in $img_paths
 	do
-		test_img=dataset/CCF-testing-Semi/${j}.png
-		let index=${j}-1
-		if [ -f  ${save_dir}/test_${j}_pred.png ] ;then
-			echo ${save_dir}/test_${j}_pred.png
-			echo "file exists"
+		file_name=$(basename $img_path)
+		# remove any suffix
+		img_name=${file_name%.*}
+		echo $img_name
+		if [ -f ${save_dir}/${img_name}_pred.png ] ;then
+			echo ${save_dir}/${img_name}_pred.png
+			echo "File exists: $img_path"
 			continue
 		fi
-		CUDA_VISIBLE_DEVICES=${gpuids[${index}]}
-		python src/processing/test.py --img_path $test_img \
-						--out_path ${save_dir}/${j}_pred.png \
-						--vis_out_path ${save_dir}/${j}_pred_vis.png \
+		echo "Testing $img_path"
+		python src/processing/test.py --img_path $img_path \
+						--out_path ${save_dir}/${img_name}_pred.png \
+						--vis_out_path ${save_dir}/${img_name}_pred_vis.png \
 						--gpu 0 \
 						--batch_size 8 \
 						--stride 64 \
-						--model_path $model \
+						--model_path $model_path \
 						--input_size 256 \
-						--crop_scales 192 224 256 288 320 \
+						--crop_scales 192 224 256 288 \
 						--tempdir ${save_dir}/temp
 	done
 done
 date
-echo "testing ended."
+echo "Testing ended."
